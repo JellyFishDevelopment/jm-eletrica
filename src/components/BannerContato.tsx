7ed985671ce65fe676b936aa8aa65140b9cec2ca
Felipe Souza <87735540+felipe-souza17@users.noreply.github.com>
2023-01-31 18:20:59 -0300

    Crianção do Banner de Contato

---
+++ src/components/BannerContato.tsx
@@ -0,0 +1,12 @@
+export default function BannerContato() {
+  return (
+    <div className="flex justify-between  items-center p-16 w-full h-96 bg-third-banner bg-cover bg-no-repeat">
+      <p className=" mx-auto font-light text-white text-4xl">
+        Entre em contato com a JM Elétrica
+      </p>
+      <button className="mx-auto text-center bg-[#969696] text-white p-6  rounded-2xl px-24 font-bold text-4xl">
+        <p>Fale conosco</p>
+      </button>
+    </div>
+  )
+}
