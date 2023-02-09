9c309dc166d39a8c8cf90a6564bd458fba931df9
Felipe Souza <87735540+felipe-souza17@users.noreply.github.com>
2023-02-09 20:22:21 -0300

    Animações na tela sobre nós

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -55,14 +55,31 @@ export default function About() {
       />
       {/* Sobre nós */}
       <div className="bg-[#969696] p-8 ">
-        <h2 className="font-bold text-6xl sm:text-8xl text-white text-center">
-          Sobre nós
-        </h2>
-        <div className="text-white p-8 sm:p-16 bg-[#B6B5B0] rounded-xl sm:w-3/4 mx-auto my-16">
+        <motion.div
+          initial={{ opacity: 0, y: 90 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: false }}
+          transition={{ duration: 0.9 }}
+        >
+          <h2 className="font-bold text-6xl sm:text-8xl text-white text-center">
+            Sobre nós
+          </h2>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 90 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: false }}
+          transition={{ duration: 1.1 }}
+          className="text-white p-8 sm:p-16 bg-[#B6B5B0] rounded-xl sm:w-3/4 mx-auto my-16"
+        >
           <h3 className="font-normal text-7xl">JM Elétrica</h3>
           <p className="font-bold text-4xl">Engenharia elétrica de qualidade</p>
           <p className="font-light text-3xl my-16">
-            Nossa visão como empresa é manter a qualidade e excelência na Engenharia Elétrica, fornecendo serviços e soluções integradas para se tornar referência no mercado, desta forma conquistando confiança e entregando cada vez mais eficiência nos serviços prestados, integrando pessoas para somar e desenvolver um futuro melhor.
+            Nossa visão como empresa é manter a qualidade e excelência na
+            Engenharia Elétrica, fornecendo serviços e soluções integradas para
+            se tornar referência no mercado, desta forma conquistando confiança
+            e entregando cada vez mais eficiência nos serviços prestados,
+            integrando pessoas para somar e desenvolver um futuro melhor.
           </p>
           <div className="text-center">
             <button
@@ -72,38 +89,70 @@ export default function About() {
               Saiba mais sobre os nossos serviços
             </button>
           </div>
-        </div>
+        </motion.div>
       </div>
       {/* Imagens elétrica */}
       <div className="p-8 bg-white flex flex-col sm:flex-row justify-evenly">
-        <img
-          className="rounded-[50px] my-2"
-          src={eletricaUm}
-          alt="Homem operando o notebook"
-        />
-        <img
-          className="rounded-[50px] my-2"
-          src={eletricaDois}
-          alt="Homem operando cabos"
-        />
+        <motion.div
+          initial={{ opacity: 0, y: -90 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: false }}
+          transition={{ duration: 0.9 }}
+        >
+          <img
+            className="rounded-[50px] my-2"
+            src={eletricaUm}
+            alt="Homem operando o notebook"
+          />
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: -90 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: false }}
+          transition={{ duration: 0.9 }}
+        >
+          <img
+            className="rounded-[50px] my-2"
+            src={eletricaDois}
+            alt="Homem operando cabos"
+          />
+        </motion.div>
       </div>
       {/* Experiência e agilidade */}
       <div className="p-8 sm:p-16 bg-[#969696]">
-        <h2 className="font-bold text-5xl sm:text-8xl text-white text-center">
-          Experiência e agilidade
-        </h2>
-        <div className="flex flex-col sm:flex-row justify-between text-white my-8">
+        <motion.div
+          initial={{ opacity: 0, y: -90 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: false }}
+          transition={{ duration: 0.9 }}
+        >
+          <h2 className="font-bold text-5xl sm:text-8xl text-white text-center">
+            Experiência e agilidade
+          </h2>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: -90 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: false }}
+          transition={{ duration: 0.6 }}
+          className="flex flex-col sm:flex-row justify-between text-white my-8"
+        >
           <p className="sm:w-[750px] text-center font-light text-2xl sm:text-4xl mx-2">
-            Me chamo Jair Melo e sou especialista em manutenção. Com 12 anos de experiência na área de manutenção industrial, atuo com projetos de melhoria industrial, comercial e laudos técnicos. 
-
-            Graduado em Engenharia Elétrica com ênfase em Eletrotécnica. Finalizando sua Pós Graduação em Gerenciamento de Projetos e Engenharia de Confiabilidade.
+            Me chamo Jair Melo e sou especialista em manutenção. Com 12 anos de
+            experiência na área de manutenção industrial, atuo com projetos de
+            melhoria industrial, comercial e laudos técnicos. Graduado em
+            Engenharia Elétrica com ênfase em Eletrotécnica. Finalizando sua Pós
+            Graduação em Gerenciamento de Projetos e Engenharia de
+            Confiabilidade.
           </p>
           <p className="sm:w-[750px] text-center font-light text-2xl sm:text-4xl mx-2">
-            Minha proposta é sempre dedicar meu tempo para entregar serviços com qualidade em tempo hábil, respeitando o meio ambiente e todos os protocolos de segurança. 
-
-            Oferecemos serviços variados de elétrica, então caso tenha alguma dúvida ou precise de algum outro serviço que não encontrou no site, entre em contato conosco!
+            Minha proposta é sempre dedicar meu tempo para entregar serviços com
+            qualidade em tempo hábil, respeitando o meio ambiente e todos os
+            protocolos de segurança. Oferecemos serviços variados de elétrica,
+            então caso tenha alguma dúvida ou precise de algum outro serviço que
+            não encontrou no site, entre em contato conosco!
           </p>
-        </div>
+        </motion.div>
       </div>
       {/* Trabalhos recentes */}
       <div className="text-center">
