3c5b71782b6072b4321239e07ce75720a7798ee8
Felipe Souza <87735540+felipe-souza17@users.noreply.github.com>
2023-02-09 20:02:24 -0300

    Alteração da altura no card de serviço

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -6,7 +6,7 @@ interface ServiceCardProps {
 }
 export default function ServiceCard(props: ServiceCardProps) {
   return (
-    <div className="bg-[#969696] w-full sm:w-80 rounded-2xl mt-4 text-center p-8">
+    <div className="bg-[#969696] w-full sm:w-80 h-full rounded-2xl mt-4 text-center p-8">
       <div className=" m-auto w-20">{props.image}</div>
       <p className="text-white mt-8 text-regular text-xl">{props.text}</p>
     </div>
